8b580eb0ef60bf716c0a84bcbba05727eeeabe61
Egor Uni <egoruni@isavin.fritz.box>
2018-10-15 17:49:44 +0200

    new path variables

--- zas_rep_tools_data/utils.py
+++ zas_rep_tools_data/utils.py
@@ -10,4 +10,10 @@
 import inspect, os
 
 #path_to_zas_rep_tools_data = inspect.getfile(inspect.currentframe()) # script filename (usually with path)
-path_to_zas_rep_tools_data = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
+path_to_data_folder = os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), "data") # script directory
+
+path_to_models = os.path.join(path_to_data_folder, "data/models") # script directory
+
+path_to_someweta_models = os.path.join(path_to_models, "SoMeWeTa")
+
+path_to_stop_words = os.path.join(path_to_data_folder, "stop_words")
